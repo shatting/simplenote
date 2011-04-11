ee8f06dfb32142b58940f1420baf08864ad0b185
Stephan Hattinger <stephan.hattinger@gmail.com>
2011-04-12 00:12:52 +0200

    options check for email change and give warning if there are unsynchronized local changes

--- javascripts/options.js
+++ javascripts/options.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-  if (localStorage) {
-    $("#email").val(localStorage.email);
-    $("#password").val(localStorage.password);    
-    localStorage.abstractlines && $("#abstractlines").val(localStorage.abstractlines);
-  }
+  
+  $("#email").val(localStorage.email);
+  $("#password").val(localStorage.password);    
+  localStorage.abstractlines && $("#abstractlines").val(localStorage.abstractlines);
+  
 });
 
 /*
@@ -11,13 +11,26 @@ $(document).ready(function() {
  * @param ms Milliseconds to fade in the status message.
  */
 function save_options(ms) {
-  if (localStorage) {
-      localStorage.email = $("#email").val();
-      localStorage.password = $("#password").val();	
-      localStorage.abstractlines = $("#abstractlines").val();	
-  }
-
   var status = $("#status");
+  var email = $("#email").val();
+  
+  if (email != localStorage.email && (localStorage._syncKeys)) {
+          if (!confirm("You are about to switch your Simplenote login!\n\nThere are notes stored locally that have not been synchronized to the server.\n\nIf you switch accounts now, those changes will be lost.\n\nContinue?")) {              
+                status.html("Not saved. Please connect to the internet to synchronize local changes to the server.");
+                status.css("opacity", 1);
+                $("#email").val(localStorage.email);
+                return;
+          }
+  }
+  
+  localStorage.clear(); 
+            
+  localStorage.email = email;
+  localStorage.password = $("#password").val();	
+  localStorage.abstractlines = $("#abstractlines").val();	
+  
+
+  
   if (localStorage && localStorage.email && localStorage.password && localStorage.abstractlines) {
     status.html("Options saved");
   } else {
