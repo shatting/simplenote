0676ec0e7d599c4d93df78d131e9e3dcdf753811
Stephan Hattinger <stephan.hattinger@gmail.com>
2011-05-01 19:54:37 +0200

    better url regexp, still one bug tho: spaces after url get uncluded

--- javascripts/lib/codemirror1/parsesimple.js
+++ javascripts/lib/codemirror1/parsesimple.js
@@ -5,8 +5,22 @@ var SimpleParser = Editor.Parser = (function() {
 //  }
   var tokenizeSimple = (function() {
 
-    var urlRe = /^(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)\.(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?[^\s\(\)\[\]\.]/;
-
+    var urlRe = /^(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)\.(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?[^\s\(\)\[\]\."'{}]/;
+    var urlRe = /^(ftp|http|https):\/\/(\w+:{0,1}\w*@)?((?:[a-z0-9](?:[a-z0-9\-]{0,61}[a-z0-9])+\.)+[a-z]{2,6}|[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})(:[0-9]{1,4})?(\/[\w#!~:.?+=&%@!\-\/]*)?/i;
+    //                                 (user:passwrd@)?((?:no "-"     (?:                              )+ .)+[tld no "-" ])(:port      )?(/ | /(path               ))?
+    //var urlRe = /^(\()((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&'()*+,;=:\/?#[\]@%]+)(\))|(\[)((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&'()*+,;=:\/?#[\]@%]+)(\])|(\{)((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&'()*+,;=:\/?#[\]@%]+)(\})|(<|&(?:lt|#60|#x3c);)((?:ht|f)tps?:\/\/[a-z0-9\-._~!$&'()*+,;=:\/?#[\]@%]+)(>|&(?:gt|#62|#x3e);)|((?:^|[^=\s'"\]])\s*['"]?|[^=\s]\s+)(\b(?:ht|f)tps?:\/\/[a-z0-9\-._~!$'()*+,;=:\/?#[\]@%]+(?:(?!&(?:gt|#0*62|#x0*3e);|&(?:amp|apos|quot|#0*3[49]|#x0*2[27]);[.!&',:?;]?(?:[^a-z0-9\-._~!$&'()*+,;=:\/?#[\]@%]|$))&[a-z0-9\-._~!$'()*+,;=:\/?#[\]@%]*)*[a-z0-9\-_~$()*+=\/#[\]@%])/i;
+    // from http://jmrware.com/articles/2010/linkifyurl/linkify.html
+    function urlInfo(urlMatch) {
+        var urlinfo = {
+            full: urlMatch[0],
+            protocol: urlMatch[1],
+            userpass: urlMatch[2],
+            domain: urlMatch[3],
+            port: urlMatch[4],
+            path: urlMatch[5]            
+        };
+        return urlinfo;
+    }
     function normal(source, setState) {
       var ch = source.peek();
       var url;
@@ -14,12 +28,15 @@ var SimpleParser = Editor.Parser = (function() {
           source.next();
           return "whitespace";
       }
-
-      if (source.lookAheadRegex(urlRe, true))
+      url = source.lookAheadRegex(urlRe, true)
+      if (url) {
+          //console.log("found a link:");
+          //console.log(urlInfo(url));
           return "sn-link";
+      }
 
       while(!url && !source.endOfLine()) {
-        source.nextWhileMatches(/[^h\n]/);
+        source.nextWhileMatches(/[^h\n\s]/);
         if (!source.endOfLine()) {
             url = source.lookAheadRegex(urlRe, false);
             if (url)
