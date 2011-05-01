9759cf6688bc73e5ac8089e1ce47aa70546af85d
Stephan Hattinger <stephan.hattinger@gmail.com>
2011-05-01 19:31:16 +0200

    improved context menus

--- javascripts/background.js
+++ javascripts/background.js
@@ -36,42 +36,212 @@ $(document).ready(function() {
     backgroundSync(true);    
 });
 
+//        Selection ###############################
+//        INFO:-------
+//        {
+//            "editable":false,
+//            "linkUrl":"http://derstandard.at/1303950559403/Sonntag-Workingmans-Death",
+//            "mediaType":"image",
+//            "menuItemId":2,
+//            "pageUrl":"http://derstandard.at/1303950558961/Sonntag-Erik-der-Wikinger",
+//            "selectionText":"23.30 bis 1.30 | ORF 2 | DOKUMENTARFILM | Ö",
+//            "srcUrl":"http://images.derstandard.at/t/1/2011/04/29/1303957348544.jpg"
+//        }
+//        TAB:--------
+//        {
+//            "favIconUrl":"http://derstandard.at/favicon.ico",
+//            "id":2,
+//            "incognito":false,
+//            "index":0,
+//            "pinned":false,
+//            "selected":true,
+//            "status":"complete",
+//            "title":"Erik, der Wikinger - Switchlist - derStandard.at › Etat",
+//            "url":"http://derstandard.at/1303950558961/Sonntag-Erik-der-Wikinger",
+//            "windowId":1
+//        }
+//
+//        IMG SRC ###############################
+//        INFO:-------
+//        {
+//            "editable":false,
+//            "linkUrl":"http://derstandard.at/1303950558961/Sonntag-Erik-der-Wikinger",
+//            "mediaType":"image",
+//            "menuItemId":4,
+//            "pageUrl":"http://derstandard.at/r2140/Switchlist",
+//            "srcUrl":"http://images.derstandard.at/t/108/2011/04/29/1303957331786.jpg"
+//        }
+//        TAB:--------
+//        {
+//            "favIconUrl":"http://derstandard.at/favicon.ico",
+//            "id":2,
+//            "incognito":false,
+//            "index":0,
+//            "pinned":false,
+//            "selected":true,
+//            "status":"complete",
+//            "title":"Switchlist - derStandard.at › Etat › Medien › TV",
+//            "url":"http://derstandard.at/r2140/Switchlist",
+//            "windowId":1
+//        }
+//
+//        Link Url: ###############################
+//        INFO:-------
+//        { 
+//            "editable":false,
+//            "linkUrl":"http://derstandard.at/1303950558961/Sonntag-Erik-der-Wikinger",
+//            "mediaType":"image",
+//            "menuItemId":7,
+//            "pageUrl":"http://derstandard.at/r2140/Switchlist",
+//            "srcUrl":"http://images.derstandard.at/t/108/2011/04/29/1303957331786.jpg"
+//        }
+//        TAB:--------
+//        {
+//            "favIconUrl":"http://derstandard.at/favicon.ico",
+//            "id":2,
+//            "incognito":false,
+//            "index":0,
+//            "pinned":false,
+//            "selected":true,
+//            "status":"complete",
+//            "title":"Switchlist - derStandard.at › Etat › Medien › TV",
+//            "url":"http://derstandard.at/r2140/Switchlist",
+//            "windowId":1
+//        }
+//
+//        Page Url:###############################
+//        INFO:-------
+//        {
+//            "editable":false,
+//            "menuItemId":1,
+//            "pageUrl":"http://derstandard.at/r2140/Switchlist"
+//        }
+//
+//        TAB:--------
+//        {
+//            "favIconUrl":"http://derstandard.at/favicon.ico",
+//            "id":2,
+//            "incognito":false,
+//            "index":0,
+//            "pinned":false,
+//            "selected":true,
+//            "status":"complete",
+//            "title":"Switchlist - derStandard.at › Etat › Medien › TV",
+//            "url":"http://derstandard.at/r2140/Switchlist",
+//            "windowId":1
+//        }
+
 // add context menus
 // contexts : Legal values are: 'all', 'page', 'selection', 'link', 'editable', 'image', 'video', and 'audio'. Defaults to ['page'].
-chrome.contextMenus.create({type:"normal",title:"Create a Simplenote (URL)",contexts:['page'],onclick:handleContextMenu})
-chrome.contextMenus.create({type:"normal",title:"Create a Simplenote (Selection)",contexts:['selection'],onclick:handleContextMenu})
-chrome.contextMenus.create({type:"normal",title:"Create a Simplenote (Link)",contexts:['link','image'],onclick:handleContextMenu})
+chrome.contextMenus.create({type:"normal",title:"Create a Simplenote (Page Url)",        contexts:['page'],
+        onclick: function(info, tab){
+            //createNoteFromBG({content:"Page Url:\n\nINFO:-------\n" + JSON.stringify(info) + "\n\nTAB:--------\n" + JSON.stringify(tab)});
+            createNoteFromBG({content:info.pageUrl});
+        }});
+chrome.contextMenus.create({type:"normal",title:"Create a Simplenote (Selection)",  contexts:['selection'],
+        onclick: function(info, tab){
+            //createNoteFromBG({content:"Selection:\n\nINFO:-------\n" + JSON.stringify(info) + "\n\nTAB:--------\n" + JSON.stringify(tab)});
+            createNoteFromBG({content:info.selectionText + "\n\n" + "[Source: " + tab.url + "]"});
+        }});
+chrome.contextMenus.create({type:"normal",title:"Create a Simplenote (Link Url)",       contexts:['link'],
+        onclick: function(info, tab){
+            //createNoteFromBG({content:"Link URL:\n\nINFO:-------\n" + JSON.stringify(info) + "\n\nTAB:--------\n" + JSON.stringify(tab)});
+            createNoteFromBG({content:info.linkUrl});
+        }});
+chrome.contextMenus.create({type:"normal",title:"Create a Simplenote (Image Url)",    contexts:['image'],
+        onclick: function(info, tab){
+            createNoteFromBG({content:info.srcUrl});
+        }});
+chrome.contextMenus.create({type:"normal",title:"Append to last open (Page Url)",        contexts:['page'],
+        onclick: function(info, tab){
+            //appendToLastOpenNoteFromBG("Append Page:\n\nINFO:-------\n" + JSON.stringify(info) + "\n\nTAB:--------\n" + JSON.stringify(tab));
+            appendToLastOpenNoteFromBG(info.pageUrl);
+        }});
+chrome.contextMenus.create({type:"normal",title:"Append to last open (Selection)",  contexts:['selection'],
+        onclick: function(info, tab){
+            //appendToLastOpenNoteFromBG("Append Selection:\n\nINFO:-------\n" + JSON.stringify(info) + "\n\nTAB:--------\n" + JSON.stringify(tab));
+            appendToLastOpenNoteFromBG(info.selectionText + "\n\n" + "[Source: " + tab.url + "]");
+        }});
+chrome.contextMenus.create({type:"normal",title:"Append to last open (Link Url)",       contexts:['link'],
+        onclick: function(info, tab){
+            //appendToLastOpenNoteFromBG("Append Link Url:\n\nINFO:-------\n" + JSON.stringify(info) + "\n\nTAB:--------\n" + JSON.stringify(tab));
+            appendToLastOpenNoteFromBG(info.linkUrl);
+        }});
+chrome.contextMenus.create({type:"normal",title:"Append to last open (Image Url)",    contexts:['image'],
+        onclick: function(info, tab){
+            appendToLastOpenNoteFromBG(info.srcUrl);
+        }});
 
 //chrome.contextMenus.create({type:"normal",title:"Insert a Simplenote",contexts:['editable'],onclick:handleContextMenu})
+//function handleContextMenu(info, tab) {
+//    var content = "";
+//    var gotSelection = info.selectionText != undefined;
+////    console.log(info)
+////    console.log(tab)
+//    if (info.linkUrl) {
+//        if (gotSelection)
+//            content += info.selectionText + ": ";
+//        content += info.linkUrl;
+//    } else {
+//        if (gotSelection) {
+//            content += info.selectionText + "\n";
+//            content += "(Source: " + tab.title + "\n" + info.pageUrl + ")";
+//        } else
+//            content += tab.title + " (" + info.pageUrl + ")";
+//    }
+//
+//    createNoteFromBG({content:content});
+//}
 
-function handleContextMenu(info, tab) {
-    var content = "";
-    var gotSelection = info.selectionText != undefined;
-//    console.log(info)
-//    console.log(tab)
-    if (info.linkUrl) {
-        if (gotSelection)
-            content += info.selectionText + ": ";
-        content += info.linkUrl;        
-    } else {
-        if (gotSelection) {
-            content += info.selectionText + "\n";
-            content += "(Source: " + tab.title + "\n" + info.pageUrl + ")";
-        } else
-            content += tab.title + " (" + info.pageUrl + ")";
-    } 
-    
-    var note = {content:content};
-    chrome.browserAction.setBadgeText({text:"..."});
-    chrome.browserAction.setBadgeBackgroundColor({color:[255,0,0,128]});
-    SimplenoteDB.createNote(note, function(note) {
-        chrome.browserAction.setBadgeText({text:"ok"});
-        chrome.browserAction.setBadgeBackgroundColor({color:[0,255,0,128]});
-        localStorage.opentonotekey = note.key;
-        //setTimeout('chrome.browserAction.setBadgeText({text:""});window.open("popup.html");', 2000);
-        setTimeout('chrome.browserAction.setBadgeText({text:""});', 2000);
+function appendToLastOpenNoteFromBG(string) {
+
+    if (!localStorage.opentonotekey || localStorage.opentonotekey == '') {
+        signalError();
+        return;
+    }    
+
+    signalProcessing();
+    SimplenoteDB.getNote(localStorage.opentonotekey,function(oldnote) {
+        oldnote.content += "\n" + string;
+        SimplenoteDB.updateNote(oldnote, function(note) {
+            if (note) {
+                localStorage.opentonotekey = note.key;
+                signalSuccess();
+            } else 
+                signalError()
+            
+        });
     });
 }
+
+function createNoteFromBG(note) {
+    signalProcessing();
+    SimplenoteDB.createNote(note, function(note) {
+        if (note) {
+            localStorage.opentonotekey = note.key;
+            signalSuccess();
+        } else
+            signalError();
+    });
+}
+
+function signalProcessing() {
+    chrome.browserAction.setBadgeText({text:"..."});
+    chrome.browserAction.setBadgeBackgroundColor({color:[255,0,0,128]});
+}
+
+function signalSuccess() {    
+    chrome.browserAction.setBadgeBackgroundColor({color:[0,255,0,128]});
+    chrome.browserAction.setBadgeText({text:"ok"});
+    setTimeout('chrome.browserAction.setBadgeText({text:""});', 2000);
+}
+
+function signalError() {
+    chrome.browserAction.setBadgeBackgroundColor({color:[255,0,0,128]});
+    chrome.browserAction.setBadgeText({text:"err"});
+    setTimeout('chrome.browserAction.setBadgeText({text:""});', 20000);
+}
+
 // selection:
 // {"editable":false,"menuItemId":1,"pageUrl":"http://derstandard.at/r2140/Switchlist","selectionText":"Kalender Girls\n20.15 bis 22.25 | Super RTL | SOZIALKOMÖDIE | Calendar Girls, GB/USA 2003, Nigel Cole"}
 // {"favIconUrl":"http://derstandard.at/favicon.ico","id":16,"incognito":false,"index":4,"pinned":false,"selected":true,"status":"complete","title":"Switchlist - derStandard.at › Etat › Medien › TV","url":"http://derstandard.at/r2140/Switchlist","windowId":1}
