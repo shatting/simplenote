bfc1095bc98a5dda76045dd2550d7fe4ac4ce122
Stephan Hattinger <stephan.hattinger@gmail.com>
2011-06-13 17:45:36 +0200

    tags api initial commit

--- javascripts/simplenoteAPI.js
+++ javascripts/simplenoteAPI.js
@@ -216,14 +216,13 @@ var SimplenoteAPI2 = {
         if (options.length) urloptions+="&length=" + options.length;
         if (options.mark) urloptions+="&mark=" + options.mark;
         if (options.since) urloptions+="&since=" + options.since;
-        var that = this;
         this.log("index::options: " + urloptions);
         jQuery.ajax({
             url: url + urloptions,
             dataType: "json",
             //timeout: 3000,
             complete: function(jqXHR, textStatus) {
-                that.log("index::status=" + textStatus + "(" + jqXHR.status + ")");
+                SimplenoteAPI2.log("index::status=" + textStatus + "(" + jqXHR.status + ")");
                 switch (jqXHR.status) {
                     case 200:
                         if (callbacks.success)
@@ -261,7 +260,7 @@ var SimplenoteAPI2 = {
             url: this.root2 + "data/" + key + this.authURLadd(),
             dataType: "json",
             complete: function(jqXHR, textStatus) {
-                SimplenoteAPI2.log("::retrieve:status=" + textStatus);
+                SimplenoteAPI2.log("retrieve::status=" + textStatus);
                 switch (jqXHR.status)
                 {
                     case 200:
@@ -297,41 +296,41 @@ var SimplenoteAPI2 = {
      * @param options.results (optional) max results to be returned
      * @param options.offset (optional) offset index (use in combination with max results for pagination)
      */
-    search: function(query, callbacks, options) {
-        if (!callbacks) callbacks = {};
-        var optionsStr = "";
-        if (options && options.results)
-            optionsStr += "&results="+options.results;
-        if (options && options.offset)
-            optionsStr += "&offset="+options.results;
-
-        jQuery.ajax({
-            url: this.root + "search" + this.authURLadd() + "&query=" + escape(query) + optionsStr,
-            dataType: "json",
-            complete: function(jqXHR, textStatus) {
-                SimplenoteAPI2.log(";;search status=" + textStatus);
-                switch (jqXHR.status)
-                {
-                    case 200:
-                        var data = jQuery.parseJSON(jqXHR.responseText);
-                        if (callbacks.success)
-                            callbacks.success(data['Response']['Results']);
-                        break;
-                    case 401:
-                        if (callbacks.loginInvalid)
-                            callbacks.loginInvalid();
-                        break;
-                    case 0:
-                        if (callbacks.timeout)
-                            callbacks.timeout(options);
-                        break;
-                    default:
-                        if (callbacks.repeat)
-                            callbacks.repeat(query,callbacks,options);
-                }
-            }
-        });
-    },
+//    search: function(query, callbacks, options) {
+//        if (!callbacks) callbacks = {};
+//        var optionsStr = "";
+//        if (options && options.results)
+//            optionsStr += "&results="+options.results;
+//        if (options && options.offset)
+//            optionsStr += "&offset="+options.results;
+//
+//        jQuery.ajax({
+//            url: this.root + "search" + this.authURLadd() + "&query=" + escape(query) + optionsStr,
+//            dataType: "json",
+//            complete: function(jqXHR, textStatus) {
+//                SimplenoteAPI2.log("search status=" + textStatus);
+//                switch (jqXHR.status)
+//                {
+//                    case 200:
+//                        var data = jQuery.parseJSON(jqXHR.responseText);
+//                        if (callbacks.success)
+//                            callbacks.success(data['Response']['Results']);
+//                        break;
+//                    case 401:
+//                        if (callbacks.loginInvalid)
+//                            callbacks.loginInvalid();
+//                        break;
+//                    case 0:
+//                        if (callbacks.timeout)
+//                            callbacks.timeout(options);
+//                        break;
+//                    default:
+//                        if (callbacks.repeat)
+//                            callbacks.repeat(query,callbacks,options);
+//                }
+//            }
+//        });
+//    },
     /**
      * permanently deletes a note from the server. note.deleted must be set to 1 for this to succeed.
      *
@@ -351,7 +350,7 @@ var SimplenoteAPI2 = {
             type: "DELETE",
             url: this.root2 + "data/" + key + this.authURLadd(),
             complete: function(jqXHR, textStatus) {
-                SimplenoteAPI2.log("::destroy status=" + textStatus);
+                SimplenoteAPI2.log("destroy status=" + textStatus);
                 switch (jqXHR.status)
                 {
                     case 200:
@@ -397,7 +396,7 @@ var SimplenoteAPI2 = {
             url: this.root2 + "data/" + note.key + this.authURLadd(),
             data: encodeURIComponent(JSON.stringify(note)),
             complete: function(jqXHR, textStatus) {
-                SimplenoteAPI2.log("::update status=" + textStatus);
+                SimplenoteAPI2.log("update status=" + textStatus);
                 switch (jqXHR.status)
                 {
                     case 200:
@@ -466,5 +465,273 @@ var SimplenoteAPI2 = {
                 }
             }
         });
+    },
+
+    tagCreate: function(tag, callbacks) {
+        if (!callbacks) callbacks = {};
+
+        if (!tag || !tag.name)
+            throw "SimplenoteAPI2::TagCreate:tag or tag.name missing.";
+
+        if (!tag.index)
+            tag.index = -1;
+        if (!tag.version)
+            tag.version = 0;
+
+        (function(thistag) {
+            jQuery.ajax({
+                type: "POST",
+
+                url: SimplenoteAPI2.root2 + "tags" + SimplenoteAPI2.authURLadd(),
+
+                data: encodeURIComponent(JSON.stringify(thistag)),
+
+                complete: function(jqXHR, textStatus) {
+                    SimplenoteAPI2.log("tagCreate status=" + textStatus);
+                    switch (jqXHR.status)
+                    {
+                        case 200:
+                            if (callbacks.success)
+                                callbacks.success(JSON.parse(jqXHR.responseText));
+                            break;
+                        case 401:
+                            if (callbacks.loginInvalid)
+                                callbacks.loginInvalid();
+                            break;
+                        case 0:
+                            if (callbacks.timeout)
+                                callbacks.timeout(thistag.name);
+                            break;
+                        default:
+                            if (callbacks.repeat)
+                                callbacks.repeat(thistag.name, callbacks);
+                    }
+                }
+            });
+        })(tag);
+    },
+
+    tagUpdate: function(tag, callbacks) {
+        if (!callbacks) callbacks = {};
+
+        if (!tag || !tag.name)
+            throw "SimplenoteAPI2::TagUpdate:tag or tag.name missing.";
+
+        (function(thistag) {
+            jQuery.ajax({
+                type: "POST",
+
+                url: SimplenoteAPI2.root2 + "tags" + SimplenoteAPI2.authURLadd(),
+
+                data: encodeURIComponent(JSON.stringify(thistag)),
+
+                complete: function(jqXHR, textStatus) {
+                    SimplenoteAPI2.log("tagUpdate status=" + textStatus);
+                    switch (jqXHR.status)
+                    {
+                        case 200:
+                            if (callbacks.success)
+                                callbacks.success(JSON.parse(jqXHR.responseText));
+                            break;
+                        case 401:
+                            if (callbacks.loginInvalid)
+                                callbacks.loginInvalid();
+                            break;
+                        case 0:
+                            if (callbacks.timeout)
+                                callbacks.timeout(thistag);
+                            break;
+                        default:
+                            if (callbacks.repeat)
+                                callbacks.repeat(thistag, callbacks);
+                    }
+                }
+            });
+        })(tag);
+    },
+
+    tagDelete: function(name, callbacks) {
+        if (!callbacks)
+            callbacks = {};
+        if (!name)
+            throw "SimplenoteAPI2::deleteTag:name missing.";
+
+        (function(thisname) {
+            jQuery.ajax({
+                type: "DELETE",
+                url: SimplenoteAPI2.root2 + "tags/" + encodeURIComponent(thisname) + SimplenoteAPI2.authURLadd(),
+                complete: function(jqXHR, textStatus) {
+                    SimplenoteAPI2.log("deleteTag status=" + textStatus);
+                    switch (jqXHR.status)
+                    {
+                        case 200:
+                            if (callbacks.success)
+                                callbacks.success(jqXHR.responseText);
+                            break;
+                        case 401:
+                            if (callbacks.loginInvalid)
+                                callbacks.loginInvalid();
+                            break;
+                        case 404:
+                            if (callbacks.noteNotExist)
+                                callbacks.noteNotExist();
+                            break;
+                        case 0:
+                            if (callbacks.timeout)
+                                callbacks.timeout(thisname);
+                            break;
+                        default:
+                            if (callbacks.repeat)
+                                callbacks.repeat(callbacks);
+                    }
+                }
+            });
+        })(name);
+    },
+
+    tagIndex: function(options,callbacks) {
+        if (!callbacks) callbacks = {};
+        if (!options) options = {};
+        var url = this.root2 + "tags" + this.authURLadd();
+        var urloptions = "";
+        if (options.length) urloptions+="&length=" + options.length;
+        this.log("tagIndex::options: " + urloptions);
+        
+        jQuery.ajax({
+            url: url + urloptions,
+            dataType: "json",
+            beforeSend: function(jqXHR, settings) {
+                console.log(jqXHR)
+                console.log(settings)
+            },
+            complete: function(jqXHR, textStatus) {
+                SimplenoteAPI2.log("tagIndex::status=" + textStatus + "(" + jqXHR.status + ")");
+                switch (jqXHR.status) {
+                    case 200:
+                        console.log(document.cookie);
+                        if (callbacks.success)
+                            callbacks.success(jQuery.parseJSON(jqXHR.responseText));
+                        break;
+                    case 401:
+                        if (callbacks.loginInvalid)
+                            callbacks.loginInvalid();
+                        break;
+                    case 0:
+                        if (callbacks.timeout)
+                            callbacks.timeout();
+                        break;
+                    default:
+                        if (callbacks.repeat)
+                            callbacks.repeat(options,callbacks);
+                }
+            }
+        });
+    },
+
+    listTags: function() {
+        SimplenoteAPI2.tagIndex({length:200},{
+            success:function(tags) {
+                tags.tags.sort(function(t1,t2) {return t1.index - t2.index})
+                for (var i = 0; i<tags.tags.length;i++)
+                    console.log("%i %s (%i)",tags.tags[i].index,tags.tags[i].name,tags.tags[i].version);
+            }});
     }
 }
+
+
+/**
+ * Cookie plugin
+ *
+ * Copyright (c) 2006 Klaus Hartl (stilbuero.de)
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+ */
+
+/**
+ * Create a cookie with the given name and value and other optional parameters.
+ *
+ * @example $.cookie('the_cookie', 'the_value');
+ * @desc Set the value of a cookie.
+ * @example $.cookie('the_cookie', 'the_value', { expires: 7, path: '/', domain: 'jquery.com', secure: true });
+ * @desc Create a cookie with all available options.
+ * @example $.cookie('the_cookie', 'the_value');
+ * @desc Create a session cookie.
+ * @example $.cookie('the_cookie', null);
+ * @desc Delete a cookie by passing null as value. Keep in mind that you have to use the same path and domain
+ *       used when the cookie was set.
+ *
+ * @param String name The name of the cookie.
+ * @param String value The value of the cookie.
+ * @param Object options An object literal containing key/value pairs to provide optional cookie attributes.
+ * @option Number|Date expires Either an integer specifying the expiration date from now on in days or a Date object.
+ *                             If a negative value is specified (e.g. a date in the past), the cookie will be deleted.
+ *                             If set to null or omitted, the cookie will be a session cookie and will not be retained
+ *                             when the the browser exits.
+ * @option String path The value of the path atribute of the cookie (default: path of page that created the cookie).
+ * @option String domain The value of the domain attribute of the cookie (default: domain of page that created the cookie).
+ * @option Boolean secure If true, the secure attribute of the cookie will be set and the cookie transmission will
+ *                        require a secure protocol (like HTTPS).
+ * @type undefined
+ *
+ * @name $.cookie
+ * @cat Plugins/Cookie
+ * @author Klaus Hartl/klaus.hartl@stilbuero.de
+ */
+
+/**
+ * Get the value of a cookie with the given name.
+ *
+ * @example $.cookie('the_cookie');
+ * @desc Get the value of a cookie.
+ *
+ * @param String name The name of the cookie.
+ * @return The value of the cookie.
+ * @type String
+ *
+ * @name $.cookie
+ * @cat Plugins/Cookie
+ * @author Klaus Hartl/klaus.hartl@stilbuero.de
+ */
+jQuery.cookie = function(name, value, options) {
+    if (typeof value != 'undefined') { // name and value given, set cookie
+        options = options || {};
+        if (value === null) {
+            value = '';
+            options.expires = -1;
+        }
+        var expires = '';
+        if (options.expires && (typeof options.expires == 'number' || options.expires.toUTCString)) {
+            var date;
+            if (typeof options.expires == 'number') {
+                date = new Date();
+                date.setTime(date.getTime() + (options.expires * 24 * 60 * 60 * 1000));
+            } else {
+                date = options.expires;
+            }
+            expires = '; expires=' + date.toUTCString(); // use expires attribute, max-age is not supported by IE
+        }
+        // CAUTION: Needed to parenthesize options.path and options.domain
+        // in the following expressions, otherwise they evaluate to undefined
+        // in the packed version for some reason...
+        var path = options.path ? '; path=' + (options.path) : '';
+        var domain = options.domain ? '; domain=' + (options.domain) : '';
+        var secure = options.secure ? '; secure' : '';
+        document.cookie = [name, '=', encodeURIComponent(value), expires, path, domain, secure].join('');
+    } else { // only name given, get cookie
+        var cookieValue = null;
+        if (document.cookie && document.cookie != '') {
+            var cookies = document.cookie.split(';');
+            for (var i = 0; i < cookies.length; i++) {
+                var cookie = jQuery.trim(cookies[i]);
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+};
